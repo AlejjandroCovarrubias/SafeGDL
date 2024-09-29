2a35ae0a536ecba9c34d6eb6e9ecefd8ad268007
Alejandro Jesus Garcia Covarrubias <alejandro.garcia6126@alumnos.udg.mx>
2024-09-28 19:12:17 -0600

    FEAT!: Cambio de mapa por maptiler y set de search bar (lugares o direcciones)

--- public/js/map.js
+++ public/js/map.js
@@ -1,25 +1,67 @@
-// Inicializamos el mapa (falta poner direcciones de forma dinamcia, ahorita pone las de cucei con zoom de 16%)
-let map = L.map('map');
+document.addEventListener("DOMContentLoaded", function() {
+    // Inicializamos el mapa (falta poner direcciones de forma dinamica, ahorita pone las de cucei con zoom de 16%)
+    let map = L.map('map');
 
-function obtenerCoordenadas() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (posicion) {
-            const lat = posicion.coords.latitude;
-            const lng = posicion.coords.longitude;
-            centrarMapa(lat, lng);
-        });
+    map.setView([20.659249, -103.325971], 16);
+
+    function obtenerCoordenadas() {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(function (posicion) {
+                const lat = posicion.coords.latitude;
+                const lng = posicion.coords.longitude;
+                actCoords(lat, lng);
+                console.log(lat,lng);
+            }, function(error) {
+                console.error("ERROR: No localizacion");
+            }, {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0 // Para que no use info del cache
+            });
+        }
     }
-}
 
-function centrarMapa(lat, lng) {
-    map.setView([lat, lng], 16);
-    L.marker([lat, lng]).addTo(map) 
-        .bindPopup('Estás aquí')
-        .openPopup();
-}
+    let marker;
 
-obtenerCoordenadas();
+    function actCoords(lat,lng){
+        if(marker){
+            map.removeLayer(marker);
+        }
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(map);
+        marker = L.marker([lat,lng]).addTo(map)
+            .bindPopup('Estás aquí')
+    }
+
+    obtenerCoordenadas();
+
+    const layer = L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{
+        tileSize: 512,
+        zoomOffset: -1,
+        minZoom: 1,
+        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+        crossOrigin: true
+      }).addTo(map);
+
+    // Control de búsqueda con Nominatim
+    const searchControl = L.Control.geocoder({
+        collapsed: false,
+        placeholder: 'Buscar...',
+        errorMessage: 'No se encontraron resultados',
+        // Función para obtener resultados enriquecidos
+        geocoder: new L.Control.Geocoder.Nominatim({
+            serviceUrl: '   https://nominatim.openstreetmap.org/',
+            params: {
+                format: 'json',
+                addressdetails: 1,
+                limit: 5, // Limita la cantidad de resultados devueltos
+            }
+        })
+    }).on('markgeocode', function(e) {
+        const latlng = e.geocode.center;
+        L.marker(latlng).addTo(map)
+            .bindPopup(e.geocode.name)
+            .openPopup();
+        map.setView(latlng, 16);
+    }).addTo(map);
+    });
+
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,9 +17,15 @@
     <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
     <link rel="stylesheet" href="{{asset('css/aos.css')}}">
     <!-- API mapa para consulta  -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-    crossorigin=""/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    
+    <!-- SEARCH BAR PLUGIN -->
+    <script src="https://cdn.maptiler.com/maptiler-geocoding-control/v1.3.3/leaflet.umd.js"></script>
+    <link href="https://cdn.maptiler.com/maptiler-geocoding-control/v1.3.3/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+    <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script> 
 
     <!-- MAIN CSS -->
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
@@ -170,11 +176,6 @@
     <script src="{{asset('js/isotope.pkgd.min.js')}}"></script>
     <script src="{{asset('js/aos.js')}}"></script>
     <script src="{{asset('js/main.js')}}"></script>
-
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-      crossorigin="">
-    </script>
     @livewireScripts
     @stack('scripts')
   </body>
