5998b01357b4d5158e70c3c1987acdbc08ae7c1f
Alejandro Jesus Garcia Covarrubias <alejandro.garcia6126@alumnos.udg.mx>
2024-09-28 20:33:11 -0600

    FIX!: Layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -90,26 +90,10 @@
         </div>
 
       </header>
+        
+    {{$slot}}
 
-    <div class="ftco-blocks-cover-1">
-      <div class="site-section-cover overlay" style="background-image: url({{asset('img/gdl-1.jpg')}})">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-md-5" data-aos="fade-right">
-              <h1 class="mb-3 text-white">SafeGDL</h1>
-              <p>Una herramienta dedicada para tu seguridad, protección y comodidad.</p>
-              <p class="d-flex align-items-center">
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="site-section py-5">
-        {{$slot}}
-    </div>
-
+    
     <div class="ftco-blocks-cover-1">
         <div class="container">    
             <div class="mb-3 row align-items-center">
